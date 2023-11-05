c6fed96ac94aaf310c29f31e4ec240e72268e5ef
Ian Smith <iansmith1026@gmail.com>
2023-11-05 15:47:34 -0700

    initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+        <nav>NAV</nav>
+        <main>
+            IAN J. SMITH
+        </main>
+        <footer>FOOTER</footer>
+    </>
+  )
+}
