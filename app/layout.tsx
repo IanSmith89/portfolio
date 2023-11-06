f9fb2dc88912f123386322fccb7bdee99f485222
Ian Smith <iansmith1026@gmail.com>
2023-11-05 17:59:40 -0700

    update home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,18 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Changa } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const changa = Changa({ subsets: ['latin'], display: 'swap', variable: '--font-changa' })
 
 export const metadata: Metadata = {
-  title: 'Ian J. Smith | UX Engineer & Designer',
-  description: 'The portfolio site for Ian J. Smith.',
+	title: 'Ian J. Smith | UX Engineer & Designer',
+	description: 'The portfolio site for Ian J. Smith.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<body className={`${changa.variable}`}>{children}</body>
+		</html>
+	)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return (
-    <>
-        <nav>NAV</nav>
-        <main>
-            IAN J. SMITH
-        </main>
-        <footer>FOOTER</footer>
-    </>
-  )
+	return (
+		<main className="w-full h-screen flex flex-col items-center justify-center text-center p-8">
+			<div className="relative w-full h-32 mb-6">
+				<Image alt="" src="/eye-jay-s-logo.svg" fill priority />
+			</div>
+			<h1 className="font-sans text-5xl font-medium mb-2">COMING SOON</h1>
+			<p className="font-sans text-base font-extralight text-gray-600 dark:text-gray-300">
+				I&apos;m excited to release the new version of ianjsmith.com in December 2023.
+			</p>
+		</main>
+	)
 }
