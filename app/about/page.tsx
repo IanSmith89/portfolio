a197f6dc66507064a80f319b4185357406285577
Ian Smith <iansmith1026@gmail.com>
2023-11-05 18:47:11 -0700

    add site footer

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+	return (
+		<div className="w-full h-full flex flex-col items-center justify-center text-center p-8">
+			<h1 className="font-sans text-5xl font-medium mb-2">ABOUT</h1>
+		</div>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
 	return (
-		<main className="w-full h-screen flex flex-col items-center justify-center text-center p-8">
+		<div className="w-full h-full flex flex-col items-center justify-center text-center p-8">
 			<div className="relative w-full h-32 mb-6">
 				<Image alt="" src="/eye-jay-s-logo.svg" fill priority />
 			</div>
-			<h1 className="font-sans text-5xl font-medium mb-2">COMING SOON</h1>
+			<h1 className="font-sans text-5xl font-medium mb-2">COMING 12/23</h1>
 			<p className="font-sans text-base font-extralight text-gray-600 dark:text-gray-300">
-				I&apos;m excited to release the new version of ianjsmith.com in December 2023.
+				I&apos;m excited to release the new version of ianjsmith.com very soon.
 			</p>
-		</main>
+		</div>
 	)
 }
