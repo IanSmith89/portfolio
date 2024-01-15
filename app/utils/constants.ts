ea173f6f4518ea20ada2c4f2fe047c7c031b2a8c
Ian Smith <iansmith1026@gmail.com>
2024-01-14 19:17:14 -0700

    add robots and sitemap

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -20,3 +20,5 @@ export const NAV_ROUTES = [
 export const WEB_FORM_KEY = '5602ac20-bcd3-48da-bd9e-51ddde3b01df'
 
 export const PROJECTS_URL = '/projects'
+
+export const ORIGIN = process.env.NODE_ENV === 'production' ? 'https://ianjsmith.com' : 'http://localhost:3000'
