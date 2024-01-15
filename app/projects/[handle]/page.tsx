f52f1a4d4ab42a0c9e51d24d6c7fb1f1a345c40c
Ian Smith <iansmith1026@gmail.com>
2024-01-14 18:12:53 -0700

    refactor case studies sections to handle dynamic content

--- app/projects/[handle]/page.tsx
+++ app/projects/[handle]/page.tsx
@@ -16,17 +16,12 @@ export default function ProjectPage({ params }: { params: { handle: string } }) 
 
 	if (!project) notFound()
 
+	const { backgroundImage, longTitle, longSubtitle, sections } = project
+
 	return (
 		<>
 			<section className="relative w-full h-[66vh] -mt-16">
-				<Image
-					alt=""
-					src={`/${project.backgroundImage}`}
-					className="object-cover h-full"
-					fill
-					priority
-					sizes="100vw"
-				/>
+				<Image alt="" src={`/${backgroundImage}`} className="object-cover h-full" fill priority sizes="100vw" />
 				<div className="absolute w-full h-full transition-colors bg-green-200 dark:bg-green-900 mix-blend-hard-light filter backdrop-blur-sm" />
 				<div className="relative container w-full h-full flex flex-col">
 					<div className="pt-20 flex justify-between">
@@ -44,167 +39,62 @@ export default function ProjectPage({ params }: { params: { handle: string } }) 
 					</div>
 					<div className="grid md:grid-cols-2 gap-8 flex-grow">
 						<div className="h-full flex flex-col justify-center gap-4">
-							<h1 className="text-3xl md:text-5xl font-medium">{project.longTitle}</h1>
-							<h2 className="md:text-xl">{project.longSubtitle}</h2>
+							<h1 className="text-3xl md:text-5xl font-medium">{longTitle}</h1>
+							<h2 className="md:text-xl">{longSubtitle}</h2>
 						</div>
 					</div>
 				</div>
 			</section>
-			<section className="bg-white dark:bg-grey-blue py-8">
-				<div className="container">
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Project Overview</h3>
-						<p className="prose text-indigo dark:text-white">
-							I had the privilege of leading the implementation of a significant redesign for the
-							public-facing e-commerce website of TPC, the industry leader in buying and selling used
-							bikes. The primary goal was to enhance the user experience, improve performance, and
-							modernize the overall look and feel of the site.
-						</p>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Objectives</h3>
-						<ul className="">
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Enhanced User Experience:</span> Improve navigation,
-								product discovery, and overall satisfaction for online shoppers.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Performance Optimization:</span> Enhance website speed and
-								responsiveness for a seamless user experience.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Modernization:</span> Redesign the user interface to align
-								with contemporary design trends and the brand image.
-							</li>
-						</ul>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">My Role</h3>
-						<p className="prose text-indigo dark:text-white">
-							As the frontend engineering lead, my responsibilities encompassed overseeing the entire
-							frontend development process, collaborating with our design and product teams, and ensuring
-							the successful implementation of the redesigned website while continuing to release new
-							features and product updates.
-						</p>
-					</div>
-				</div>
-			</section>
-			<section className="py-8">
-				<div className="container">
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Challenges</h3>
-						<ul className="">
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Outdated User Interface:</span> The existing website had
-								an outdated design that didn&apos;t resonate with the company&apos;s progressive image.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Suboptimal User Journey:</span> Users faced difficulties
-								navigating and finding products efficiently.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Performance Concerns:</span> Load times were inconsistent,
-								impacting the overall user experience.
-							</li>
-						</ul>
-					</div>
-				</div>
-			</section>
-			<h2 className="bg-teal-100 dark:bg-grey-blue font-light text-4xl md:text-5xl text-center py-16">
-				SOLUTION
-			</h2>
-			<section className="bg-white dark:bg-grey-blue py-8">
-				<div className="container">
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Key Features</h3>
-						<ul className="">
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Streamlined Navigation:</span> Implemented a simplified
-								menu structure for easier navigation.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Enhanced Product Pages:</span> Improved layout and
-								presentation of product information for a better user experience.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Responsive Design:</span> Ensured a seamless experience
-								across various devices.
-							</li>
-						</ul>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Technology Highlights</h3>
-						<ul className="">
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">#1:</span> Foo
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">#2:</span> Bar
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">#3:</span> Baz
-							</li>
-						</ul>
-					</div>
-				</div>
-			</section>
-			<h2 className="bg-teal-100 dark:bg-grey-blue font-light text-4xl md:text-5xl text-center py-16">
-				RESULTS &amp; IMPACT
-			</h2>
-			<section className="bg-white dark:bg-grey-blue py-8">
-				<div className="container">
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Metrics</h3>
-						<ul className="">
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Page Load Time:</span> Reduced by 40%, contributing to a
-								significant improvement in bounce rates.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Conversion Rate:</span> Increased by 25% within the first
-								month post-launch.
-							</li>
-							<li className="prose text-indigo dark:text-white mb-2">
-								<span className="font-medium">Mobile Traffic:</span> Saw a 20% increase, indicating
-								improved mobile responsiveness.
-							</li>
-						</ul>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Impact</h3>
-						<p className="prose text-indigo dark:text-white">
-							The redesign not only met but exceeded the project objectives, resulting in a more engaging
-							and efficient e-commerce platform.
-						</p>
-					</div>
-				</div>
-			</section>
-			<section className="py-8">
-				<div className="container">
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Reflection</h3>
-						<p className="prose text-indigo dark:text-white">
-							The successful implementation of this project reinforced the importance of collaboration
-							between design and development teams. It highlighted the value of user feedback throughout
-							the development process.
-						</p>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Lessons Learned</h3>
-						<p className="prose text-indigo dark:text-white">
-							Overcoming technical challenges related to performance optimization taught us the importance
-							of balancing aesthetic improvements with maintaining a fast and efficient website.
-						</p>
-					</div>
-					<div className="mb-6">
-						<h3 className="font-medium text-2xl md:text-3xl mb-4">Future Recommendations</h3>
-						<p className="prose text-indigo dark:text-white">
-							For future projects, I recommend continuous user testing throughout development and
-							increased focus on mobile-first design principles to adapt to evolving user preferences.
-						</p>
-					</div>
-				</div>
-			</section>
+			{sections?.map((section, i) => {
+				if (section.type === 'container')
+					return (
+						<section
+							key={i}
+							className={`py-8 ${section.background === 'light' ? 'bg-white dark:bg-grey-blue/50' : ''}`}
+						>
+							<div className="container">
+								{section.content.map((contentBlock) => {
+									return (
+										<div key={contentBlock.title} className="mb-6">
+											<h3 className="font-medium text-2xl md:text-3xl mb-4">
+												{contentBlock.title}
+											</h3>
+											{contentBlock.type === 'text'
+												? contentBlock.content.map((p) => (
+														<p key={p} className="prose text-indigo dark:text-white">
+															{p}
+														</p>
+												  ))
+												: null}
+											{contentBlock.type === 'list' ? (
+												<ul>
+													{contentBlock.content.map((li) => (
+														<li
+															key={li.title}
+															className="prose text-indigo dark:text-white mb-2"
+														>
+															<span className="font-medium">{li.title}:</span> {li.detail}
+														</li>
+													))}
+												</ul>
+											) : null}
+										</div>
+									)
+								})}
+							</div>
+						</section>
+					)
+
+				if (section.type === 'header')
+					return (
+						<section
+							key={i}
+							className="bg-teal-100 dark:bg-grey-blue py-16 flex items-center justify-center"
+						>
+							<h2 className="font-light text-4xl md:text-5xl">{section.title}</h2>
+						</section>
+					)
+			})}
 		</>
 	)
 }
--- app/utils/projects.ts
+++ app/utils/projects.ts
@@ -1,10 +1,39 @@
+export type TextBlock = {
+	content: string[]
+	title: string
+	type: 'text'
+}
+
+export type ListItem = {
+	detail: string
+	title: string
+}
+
+export type ListBlock = {
+	content: ListItem[]
+	title: string
+	type: 'list'
+}
+
+export type ContainerProjectSection = {
+	background: 'light' | 'dark'
+	type: 'container'
+	content: (TextBlock | ListBlock)[]
+}
+
+export type HeaderProjectSection = {
+	type: 'header'
+	title: string
+}
+
 export type Project = {
 	backgroundImage: string
 	handle: string
+	longSubtitle?: string
 	longTitle?: string
+	sections?: (ContainerProjectSection | HeaderProjectSection)[]
 	shortSubtitle?: string
 	shortTitle: string
-	longSubtitle?: string
 }
 
 export const PROJECTS_URL: string = '/projects'
@@ -15,6 +44,178 @@ export const PROJECTS: Project[] = [
 		handle: 'tpc-rebrand',
 		longSubtitle: 'Leading the technical efforts to rebrand, optimize, and modernize the TPC e-commerce website',
 		longTitle: 'Rebranding TPC: A Lesson in Engineering Leadership',
+		sections: [
+			{
+				background: 'light',
+				type: 'container',
+				content: [
+					{
+						type: 'text',
+						title: 'Project Overview',
+						content: [
+							'I had the privilege of leading the implementation of a significant redesign for the public-facing e-commerce website of TPC, the industry leader in buying and selling used bikes. The primary goal was to enhance the user experience, improve performance, and modernize the overall look and feel of the site.',
+						],
+					},
+					{
+						type: 'list',
+						title: 'Objectives',
+						content: [
+							{
+								title: 'Enhanced User Experience',
+								detail: 'Improve navigation, product discovery, and overall satisfaction for online shoppers.',
+							},
+							{
+								title: 'Performance Optimization',
+								detail: 'Enhance website speed and responsiveness for a seamless user experience.',
+							},
+							{
+								title: 'Modernization',
+								detail: 'Redesign the user interface to align with contemporary design trends and the brand image.',
+							},
+						],
+					},
+					{
+						type: 'text',
+						title: 'My Role',
+						content: [
+							'As the frontend engineering lead, my responsibilities encompassed overseeing the entire frontend development process, collaborating with our design and product teams, and ensuring the successful implementation of the redesigned website while continuing to release new features and product updates.',
+						],
+					},
+				],
+			},
+			{
+				background: 'dark',
+				type: 'container',
+				content: [
+					{
+						type: 'list',
+						title: 'Challenges',
+						content: [
+							{
+								title: 'Outdated User Interface',
+								detail: "The existing website had an outdated design that didn't resonate with the company's progressive image.",
+							},
+							{
+								title: 'Suboptimal User Journey',
+								detail: 'Users faced difficulties navigating and finding products efficiently.',
+							},
+							{
+								title: 'Performance Concerns',
+								detail: 'Load times were inconsistent, impacting the overall user experience.',
+							},
+						],
+					},
+				],
+			},
+			{
+				type: 'header',
+				title: 'Solution',
+			},
+			{
+				background: 'light',
+				type: 'container',
+				content: [
+					{
+						type: 'list',
+						title: 'Key Features',
+						content: [
+							{
+								title: 'Streamlined Navigation',
+								detail: 'Implemented a simplified menu structure for easier navigation.',
+							},
+							{
+								title: 'Enhanced Product Pages',
+								detail: 'Improved layout and presentation of product information for a better user experience.',
+							},
+							{
+								title: 'Responsive Design',
+								detail: 'Ensured a seamless experience across various devices.',
+							},
+						],
+					},
+					{
+						type: 'list',
+						title: 'Technology Highlights',
+						content: [
+							{
+								title: '#1',
+								detail: 'Foo',
+							},
+							{
+								title: '#2',
+								detail: 'Bar',
+							},
+							{
+								title: '#3',
+								detail: 'Baz',
+							},
+						],
+					},
+				],
+			},
+			{
+				type: 'header',
+				title: 'Results & Impact',
+			},
+			{
+				background: 'light',
+				type: 'container',
+				content: [
+					{
+						type: 'list',
+						title: 'Metrics',
+						content: [
+							{
+								title: 'Page Load Time',
+								detail: 'Reduced by 40%, contributing to a significant improvement in bounce rates.',
+							},
+							{
+								title: 'Conversion Rate',
+								detail: 'Increased by 25% within the first month post-launch.',
+							},
+							{
+								title: 'Mobile Traffic',
+								detail: 'Saw a 20% increase, indicating improved mobile responsiveness.',
+							},
+						],
+					},
+					{
+						type: 'text',
+						title: 'Impact',
+						content: [
+							'The redesign not only met but exceeded the project objectives, resulting in a more engaging and efficient e-commerce platform.',
+						],
+					},
+				],
+			},
+			{
+				background: 'dark',
+				type: 'container',
+				content: [
+					{
+						type: 'text',
+						title: 'Reflection',
+						content: [
+							'The successful implementation of this project reinforced the importance of collaboration between design and development teams. It highlighted the value of user feedback throughout the development process.',
+						],
+					},
+					{
+						type: 'text',
+						title: 'Lessons Learned',
+						content: [
+							'Overcoming technical challenges related to performance optimization taught us the importance of balancing aesthetic improvements with maintaining a fast and efficient website.',
+						],
+					},
+					{
+						type: 'text',
+						title: 'Future Recommendations',
+						content: [
+							'For future projects, I recommend continuous user testing throughout development and increased focus on mobile-first design principles to adapt to evolving user preferences.',
+						],
+					},
+				],
+			},
+		],
 		shortSubtitle: 'E-Commerce Rebrand',
 		shortTitle: 'TPC',
 	},
