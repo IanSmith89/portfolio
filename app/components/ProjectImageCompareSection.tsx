10f13ff1069e80199273b2ee62a6b51fb9fb9fb1
Ian Smith <iansmith1026@gmail.com>
2024-01-17 12:47:15 -0700

    finish mobile image slider

--- app/components/ProjectImageCompareSection.tsx
+++ app/components/ProjectImageCompareSection.tsx
@@ -1,15 +1,27 @@
+'use client'
+
 import Image from 'next/image'
+import { useState } from 'react'
 import { ImageCompareProjectSection } from '@/utils/types'
 import phone from '@public/phone.svg'
 import macbook from '@public/macbook.svg'
+import Button from '@/lib/Button'
+import BackArrowIcon from '@/lib/BackArrowIcon'
+import ForwardArrowIcon from '@/lib/ForwardArrowIcon'
 
 export default function ProjectImageCompareSection({ section }: { section: ImageCompareProjectSection }) {
+	const [rangeValue, setRangeValue] = useState<number>(50)
+
+	const handleRangeChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+		setRangeValue(Number.parseInt(e.target.value, 10))
+	}
+
 	return (
 		<section className={`py-8 ${section.background === 'light' ? 'bg-white dark:bg-grey-blue/50' : ''}`}>
 			<div className="container">
+				<h3 className="font-medium text-2xl md:text-3xl mb-4">Before & After</h3>
 				<div className="relative">
-					<h3 className="font-medium text-2xl md:text-3xl mb-4">Before & After</h3>
-					<div className="absolute aspect-[95/196] bg-red w-[95%] left-[2.5%] top-[10.5%] rounded-[32px] overflow-y-auto">
+					<div className="absolute aspect-[95/196] bg-red w-[95%] left-[2.5%] top-[5%] rounded-[32px] overflow-y-auto">
 						<Image
 							alt=""
 							src={`/${section.content.mobile.image1}`}
@@ -20,10 +32,53 @@ export default function ProjectImageCompareSection({ section }: { section: Image
 						<Image
 							alt=""
 							src={`/${section.content.mobile.image2}`}
-							className="absolute top-0 left-0 w-full h-auto [clip-path:inset(0_0_0_50%)]"
+							className="absolute top-0 left-0 w-full h-auto"
+							style={{
+								clipPath: `inset(0 0 0 ${rangeValue}%)`,
+							}}
 							width={800}
 							height={13887}
 						/>
+						<div className="w-full h-full sticky top-0 overflow-x-hidden">
+							<div
+								className="absolute w-px bg-indigo/25 h-full"
+								style={{
+									left: `${rangeValue}%`,
+								}}
+							/>
+							<Button
+								className="absolute top-1/2 -translate-x-1/2 -translate-y-1/2"
+								size="large"
+								style={{
+									left: `${rangeValue}%`,
+								}}
+							>
+								<BackArrowIcon className="w-6" />
+								<ForwardArrowIcon className="w-6" />
+							</Button>
+							<input
+								className="absolute top-1/2 -translate-y-1/2 w-full bg-transparent cursor-pointer appearance-none pointer-events-none focus:outline-none
+
+                                [&::-webkit-slider-thumb]:pointer-events-auto
+                                [&::-webkit-slider-thumb]:w-[98px]
+                                [&::-webkit-slider-thumb]:h-[54px]
+                                [&::-webkit-slider-thumb]:appearance-none
+                                [&::-webkit-slider-thumb]:rounded-full
+                                
+                                [&::-moz-range-thumb]:pointer-events-auto
+                                [&::-moz-range-thumb]:w-[98px]
+                                [&::-moz-range-thumb]:h-[54px]
+                                [&::-moz-range-thumb]:appearance-none
+                                [&::-moz-range-thumb]:rounded-full
+                                "
+								type="range"
+								min="0"
+								max="100"
+								onChange={handleRangeChange}
+								value={rangeValue}
+								aria-label="Percentage of before photo shown"
+							/>
+						</div>
 					</div>
 					<Image alt="" src={phone} className="relative md:hidden w-full h-auto pointer-events-none" />
 					<Image
